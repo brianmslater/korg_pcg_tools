0fce572ba11ba99f4468147e0e24a563f020d899
Brian Slater <brian.m.slater@gmail.com>
2025-11-16 20:04:54 -0500

    Update repository name to korg_pcg_tools

--- setup.py
+++ setup.py
@@ -13,11 +13,11 @@ setup(
     description="Cross-platform Korg PCG file editor - Python rewrite of PCG Tools",
     long_description=long_description,
     long_description_content_type="text/markdown",
-    url="https://github.com/brianmslater/pcg-tools-python",
+    url="https://github.com/brianmslater/korg_pcg_tools",
     project_urls={
-        "Bug Reports": "https://github.com/brianmslater/pcg-tools-python/issues",
-        "Source": "https://github.com/brianmslater/pcg-tools-python",
-        "Documentation": "https://github.com/brianmslater/pcg-tools-python#readme",
+        "Bug Reports": "https://github.com/brianmslater/korg_pcg_tools/issues",
+        "Source": "https://github.com/brianmslater/korg_pcg_tools",
+        "Documentation": "https://github.com/brianmslater/korg_pcg_tools#readme",
     },
     packages=find_packages(),
     classifiers=[
